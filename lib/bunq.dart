1a72c545c00463592c4391d5e272c462c747cc38
Jeremiah Ogbomo <jeremiahogbomo@gmail.com>
2019-09-22 12:24:51 +0200

    chore: Http & Api abstraction

--- lib/bunq.dart
+++ lib/bunq.dart
@@ -1,6 +1,6 @@
 library bunq;
 
 export 'src/Bunq.dart';
-export 'src/Devices.dart';
-export 'src/Installations.dart';
-export 'src/Sessions.dart';
+export 'src/api/Devices.dart';
+export 'src/api/Installations.dart';
+export 'src/api/Sessions.dart';
--- lib/src/api/Devices.dart
+++ lib/src/api/Devices.dart
@@ -1,15 +1,11 @@
 import 'package:bunq/src/Bunq.dart';
 import 'package:bunq/src/models/Device.dart';
+import 'package:bunq/src/utils/api.dart';
 import 'package:bunq/src/utils/endpoints.dart';
-import 'package:bunq/src/utils/http_wrapper.dart';
 import 'package:bunq/src/utils/log.dart';
 import 'package:bunq/src/utils/response.dart';
 
-class Devices {
-  Devices() : _http = HttpWrapper();
-
-  final HttpWrapper _http;
-
+class Devices extends Api {
   Future<Response<Device>> register(String token) async {
     final payload = <String, dynamic>{
       "secret": Bunq().apiKey,
@@ -19,7 +15,7 @@ class Devices {
     Log().debug("$runtimeType.register()", payload);
 
     final _response = Response<Device>(
-      await _http.post(Endpoints.devices, payload, shouldSign: true, token: token),
+      await http.post(Endpoints.devices, payload, shouldSign: true, token: token),
       onTransform: Device.fromJson,
     );
 
